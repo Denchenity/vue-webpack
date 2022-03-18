dc5cbc7e958af73d3df0d548b0ce17a081852ffb
Denis Drozdov <d.drozdov1996@mail.ru>
2022-03-18 23:33:41 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CssMin = require('mini-css-extract-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+//добавить DefinePlugin из webpack для настройки VUE_DEV_TOOL
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const IsProd = !isDev;
+
+const pathSrc = './web/webpack/src/';
+const pathDist = './web/webpack/dist/';
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: path.resolve(__dirname, pathSrc),
+    },
+    output: {
+        path: path.resolve(__dirname, pathDist),
+        filename: '[name].js',
+        clean: true,
+    },
+    devtool: isDev ? 'source-map': false,
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CssMin({
+            filename: '[name].css',
+        }),
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            // CSS, PostCSS, Sass
+            {
+                test:/\.scss$/i,
+                use: [
+                    CssMin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            },
+            //Шрифты
+            {
+                test: /\.ttf$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[name][ext]'
+                }
+            },
+            //vue
+            {
+                test: /\.vue$/,
+                use:'vue-loader'
+            }
+        ],
+    },
+}
